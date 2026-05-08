2f276e287b67c5a3b1a656d62e26f31a30e675ca
luissalinas54 <luisasb1712@gmail.com>
2026-05-08 18:00:29 -0500

    Manejo de eventos

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,17 +1,28 @@
 type Props = {
-  data : string[]
-}
+  data: string[];
+};
 
-function List({data}: Props) {
+//funcion que al dar click en un elemento de la lista, muestra el valor en consola
+function List({ data }: Props) {
+  const handleClick = (e: string) => {
+    console.log(e);
+  };
+
   return (
+    // map() recorre el arreglo y genera un elemento por cada dato
     <ul className="list-group">
-      // map() recorre el arreglo y genera un elemento por cada dato
       {data.map((elemento) => (
         // key identifica de forma única cada elemento para que React lo actualice correctamente
-        <li key = {elemento} className="list-group-item">{elemento}</li>
+        <li
+          onClick={() => handleClick(elemento)}
+          key={elemento}
+          className="list-group-item"
+        >
+          {elemento}
+        </li>
       ))}
     </ul>
   );
 }
 
-export default List
+export default List;
