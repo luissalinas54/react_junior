c0ae569a2887ed8fd3f27387f6f2f8db3ff2e8cd
luissalinas54 <luisasb1712@gmail.com>
2026-05-07 23:51:10 -0500

    Primer componente Card

---
+++ src/components/Card.tsx
@@ -0,0 +1,29 @@
+function Card(){
+  return (
+    <div 
+      className="card" 
+      style={{
+        width : "350px"
+      }}
+    >
+      <div className="card-body"> <CardBody /> </div>
+    </div>
+  );
+}
+
+function CardBody(){
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the
+        bulk of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
+  );
+}
+
+export default Card;
