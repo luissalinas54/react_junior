eba392fd8161492a694b8ee9c2b02d7821ba46c2
luissalinas54 <luisasb1712@gmail.com>
2026-05-09 14:18:40 -0500

    Renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import List from "./components/List";
 
 function App() {
   const list = ["gato", "perro", "conejo", "hamster", "pez"];
+  //const list: string[] = [];
 
   const handleSelect = (elemento: string) => {
     console.log("Elemento seleccionado: ", elemento);
   };
+
+  //RENDERIZADO CONDICIONAL PARA QUE MUESTRE UNA LISTA SI SO LONGITUD DE ELEMENTOS ES MAYOR A 0,
+  //DE LO CONTRARIO MUESTRA UN MENSAJE
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    <p>No hay elementos en la lista</p>
+  );
   return (
     <Card>
       <CardBody
-        title="Este es el titulo"
-        text="Este es el contenido de la tarjeta que vamos a utilizar"
+        title="Esta es mi Lista"
+        //text="Este es el contenido de la tarjeta que vamos a utilizar"
       />
-      <List data={list} onSelect={handleSelect} />
+      {contenido}
     </Card>
   );
 }
